0026970c00ff6709aa418af57be50932c4a5cb5b
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2021-05-26 22:07:53 +0000

    done microfrontend

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+module.exports = {
+    webpack: (config, env) => {
+      config.optimization.runtimeChunk = false;
+      config.optimization.splitChunks = {
+        cacheGroups: {
+          default: false,
+        },
+      };
+
+      config.externals = {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+      }
+      
+      config.output.filename = "static/js/[name].js";
+  
+      config.plugins[5].options.filename = "static/css/[name].css";
+      config.plugins[5].options.moduleFilename = () => "static/css/main.css";
+      return config;
+    },
+  };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.renderDogs = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId),
+  );
+};
+
+window.unmountDogs = containerId => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+if (!document.getElementById('Dogs-container')) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
